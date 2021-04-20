062ef5948b13bfd863b2fd71e10fd0d8591dff48
khanh_intershipwata <khanh.nguyen.intership@watasoutions.com>
2021-04-20 14:46:37 +0700

    Update model.py

--- app/model.py
+++ app/model.py
@@ -11,6 +11,7 @@ class Department(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), unique=True)
     description = db.Column(db.String(200))
+    description1 = db.Column(db.String(200))
 
     def __repr__(self):
         return '<Department: {}>'.format(self.name)
