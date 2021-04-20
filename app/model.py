c982f3134cce405d618daf37c66007236837438d
khanh_intershipwata <khanh.nguyen.intership@watasoutions.com>
2021-04-20 14:50:59 +0700

    Update model.py

--- app/model.py
+++ app/model.py
@@ -16,3 +16,17 @@ class Department(db.Model):
     def __repr__(self):
         return '<Department: {}>'.format(self.name)
 
+
+class Role(db.Model):
+    """
+    Create a Role table
+    """
+
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+
+    def __repr__(self):
+        return '<Role: {}>'.format(self.name)
