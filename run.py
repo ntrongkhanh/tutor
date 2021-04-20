2381990d3f5d11ca5a9268ead01e5446e534f272
khanh_intershipwata <khanh.nguyen.intership@watasoutions.com>
2021-04-20 14:54:59 +0700

    a

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from app import create_app
 from flask_script import Manager
 from flask_migrate import MigrateCommand
 
-config_name = os.getenv('FLASK_CONFIG', 'production')
+config_name = os.getenv('FLASK_CONFIG', 'development')
 
 app = create_app(config_name)
 manager = Manager(app)
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
+export FLASK_APP=run.py
+export FLASK_CONFIG=production
 python run.py db init
 python run.py db migrate
 python run.py db upgrade
-export FLASK_APP=run.py
-export FLASK_CONFIG=production
+
 flask run -h 0.0.0.0
