b628675e58f6f2881ab46b7ccd31bfac5fdd7c7a
khanh_intershipwata <khanh.nguyen.intership@watasoutions.com>
2021-04-20 14:52:44 +0700

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from app import create_app
 from flask_script import Manager
 from flask_migrate import MigrateCommand
 
-config_name = os.getenv('FLASK_CONFIG', 'development')
+config_name = os.getenv('FLASK_CONFIG', 'production')
 
 app = create_app(config_name)
 manager = Manager(app)
